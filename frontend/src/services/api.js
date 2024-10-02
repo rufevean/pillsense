ee3ccaea5a8de0ab8a6c96090ad0ce877932232b
rufevean <deerajmydearalways@gmail.com>
2024-10-02 22:55:52 +0530

    Haskell implementation

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = import.meta.env.VITE_API_URL;
 
 // Set up Axios interceptor to include token in headers
 axios.interceptors.request.use(
