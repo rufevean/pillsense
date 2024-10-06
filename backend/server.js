5a6d89ebe55a291301c36be35e5b6f4df3f3ae48
rufevean <deerajmydearalways@gmail.com>
2024-10-06 23:04:58 +0530

    minor changes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const cors = require('cors');
-const path = require('path');
 require('dotenv').config();
 const authenticateToken = require('./middleware/auth');
 
@@ -11,40 +10,46 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// Configure CORS middleware
 app.use(cors({
-    origin: "https://pillsense-1.onrender.com", // Allow requests from this origin
-    credentials: true // Allow credentials (cookies, authorization headers, etc.)
+    origin: "https://pillsense-1.onrender.com",
+    credentials: true
 }));
-
 app.use(express.json());
 
+app.post('/test', (req, res) => {
+    console.log("Test route hit");
+    res.send('Hello World');
+});
+
 const authRoutes = require('./routes/authRoutes');
-const userRoutes = require('./routes/userRoutes');
-const dispenserRoutes = require('./routes/dispenserRoutes');
-
 app.use('/api/auth', authRoutes);
-app.use('/api/users', authenticateToken, userRoutes);
-app.use('/api/dispensers', authenticateToken, dispenserRoutes);
-
-app.get('/api', (req, res) => {
-    res.json({ message: 'Welcome to the API' });
-});
-
-// Handle client-side routing, return index.html for all other routes
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
 
 async function run() {
     try {
         await client.connect();
         console.log('Connected to the database');
+
         app.locals.client = client;
+
+        const userRoutes = require('./routes/userRoutes');
+        const dispenserRoutes = require('./routes/dispenserRoutes');
+
+        app.use('/api/users', authenticateToken, userRoutes);
+        app.use('/api/dispensers', authenticateToken, dispenserRoutes);
+
+        app.get('/api', (req, res) => {
+            res.json({ message: 'Welcome to the API' });
+        });
+
+        // Handle client-side routing, return index.html for all other routes
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, 'build', 'index.html'));
+        });
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+
     } catch (error) {
         console.error('Failed to connect to the database:', error);
     }
